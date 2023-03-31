0bbaf2599d412c546a9eebc850967b805e1a1656
Janika <janika.lenz@stud.tu-darmstadt.de>
2023-03-31 13:32:22 +0200

    add DailyWeather and DailyWeatherTest

---
+++ src/main/java/de/exxcellent/challenge/DailyWeather.java
@@ -0,0 +1,17 @@
+package de.exxcellent.challenge;
+
+public class DailyWeather {
+	public int day;
+	public int maxToday;
+	public int minToday;
+	
+	public DailyWeather(int day, int maxToday, int minToday) {
+		this.day = day;
+		this.maxToday = maxToday;
+		this.minToday = minToday;
+	}
+	
+	public int difference() {
+		return maxToday-minToday;
+	}
+}
---
+++ src/test/java/de/exxcellent/challenge/DailyWeatherTest.java
@@ -0,0 +1,15 @@
+package de.exxcellent.challenge;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DailyWeatherTest {
+
+	@Test
+	void test() {
+		DailyWeather day = new DailyWeather(2,20,12);
+		assertEquals(8,day.difference());
+	}
+
+}
